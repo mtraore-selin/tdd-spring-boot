811158ca088fdd71113cdb6b4349f62725dcb3f4
Mohamed TRAORE <mtraore@Mohameds-Air.lan>
2025-07-11 16:42:28 +0200

    chore: add readme file

--- src/main/java/com/social/platform/social/repositories/UserRepository.java
+++ src/main/java/com/social/platform/social/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.social.platform.social.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Specify here the custom method yo want
 }
--- src/main/java/com/social/platform/social/services/UserService.java
+++ src/main/java/com/social/platform/social/services/UserService.java
@@ -12,6 +12,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void registerUser(User user) {
+        // Save here
         userRepository.save(user);
     }
 }
